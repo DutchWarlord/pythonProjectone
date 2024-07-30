ca7c45c39e08fada1d89bdcab405669c7ad6d4c4
DutchWarlord <stefan.lesperance12@gmail.com>
2024-07-30 11:11:34 -0400

    While Loop completed

---
+++ project/todoloop.py
@@ -0,0 +1,15 @@
+# functions
+
+user_prompt = "Enter a todo: "
+
+todos = []
+
+while True:
+    todo = input(user_prompt)
+    print(todo.capitalize())
+    todos.append(todo)
+
+
+# append - adds items to a list object
+
+# capitalize - capitalizes the first letter of the word
