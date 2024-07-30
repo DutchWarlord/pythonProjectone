d9b61cda1e60a78b7ecd0bf8e1e295e91cceb07d
DutchWarlord <stefan.lesperance12@gmail.com>
2024-07-30 10:57:13 -0400

    Second Mini Function

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+# Structure is as follows:
+#  Imports
+#  Classes
+#  Functions
+#  MAIN FUNCTION / DEF
+#  Mainline of code
+
+
+
+
 # User Input and Variables
 # Double Quotes for Text
 print("Please enter your to do list")
@@ -6,8 +16,9 @@ user_prompt = "Enter an item:"
 todo1 = input(user_prompt)
 todo2 = input(user_prompt)
 todo3 = input(user_prompt)
+todo4 = input(user_prompt)
 
-todos = [todo1, todo2, todo3]
+todos = [todo1, todo2, todo3, todo4]
 
 print(todos)
 print(type(todos))
