581667e533fed09d948217344011785ef0c6f902
DutchWarlord <stefan.lesperance12@gmail.com>
2024-07-30 09:19:31 -0400

    Learning how to use GIT

---
+++ main.py
@@ -0,0 +1,13 @@
+# User Input and Variables
+# Double Quotes for Text
+print("Please enter your to do list")
+user_prompt = "Enter an item:"
+
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todos = [todo1, todo2, todo3]
+
+print(todos)
+print(type(todos))
